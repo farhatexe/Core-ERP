16652b658efd6730fdb3204821a3c39c389172fc
abhi-cognitivo <abhi@cognitivo.in>
2018-12-03 09:13:38 -0300

    Create ItemTransfer.cs

---
+++ Core/Models/ItemTransfer.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Runtime.Serialization;
+
+namespace Core.Models
+{
+    public class ItemTransfer
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int localId { get; set; }
+
+        [DataMember]
+        /// <summary>
+        /// Gets or sets the cloud identifier.
+        /// </summary>
+        /// <value>The cloud identifier.</value>
+        public int? cloudId { get; set; }
+
+        [DataMember]
+        /// <summary>
+        /// Gets or sets the company.
+        /// </summary>
+        /// <value>The company.</value>
+        public Company company { get; set; }
+
+        /// <summary>
+        /// Gets or sets the origin.
+        /// </summary>
+        /// <value>The origin.</value>
+        [DataMember]
+        public Location origin { get; set; }
+
+        /// <summary>
+        /// Gets or sets the destination.
+        /// </summary>
+        /// <value>The destination.</value>
+        [DataMember]
+        public Location destination { get; set; }
+
+        [DataMember]
+        /// <summary>
+        /// Gets or sets the item.
+        /// </summary>
+        /// <value>The item.</value>
+        public Item item { get; set; }
+
+        [DataMember]
+        /// <summary>
+        /// Gets or sets the date.
+        /// </summary>
+        /// <value>The date.</value>
+        public DateTime date { get; set; }
+
+        [DataMember]
+        /// <summary>
+        /// Gets or sets the debit.
+        /// </summary>
+        /// <value>The debit.</value>
+        public decimal debit { get; set; }
+
+        [DataMember]
+        /// <summary>
+        /// Gets or sets the credit.
+        /// </summary>
+        /// <value>The credit.</value>
+        public decimal credit { get; set; }
+
+        [DataMember]
+        /// <summary>
+        /// Gets or sets the comment.
+        /// </summary>
+        /// <value>The comment.</value>
+        public string comment { get; set; }
+
+        [DataMember]
+        /// <summary>
+        /// Gets or sets the create date.
+        /// </summary>
+        /// <value>The create date.</value>
+        public DateTime? createdAt { get; set; }
+
+        [DataMember]
+        /// <summary>
+        /// Gets or sets the create date.
+        /// </summary>
+        /// <value>The create date.</value>
+        public DateTime? updatedAt { get; set; }
+
+        [DataMember]
+        /// <summary>
+        /// Gets or sets the deleted at.
+        /// </summary>
+        /// <value>The deleted at.</value>
+        public DateTime? deletedAt { get; set; }
+
+        [NotMapped]
+        /// <summary>
+        /// Gets or sets the action.
+        /// </summary>
+        /// <value>The action.</value>
+        public int action { get; set; }
+    }
+}
