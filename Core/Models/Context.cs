9ac96e55bdd4b87358eff40ea991f8fdd1d1d7f9
abhi-cognitivo <abhi@cognitivo.in>
2018-12-03 21:55:26 -0300

    Added ItemTransfers to Context for Migration. Run Migration again.

--- Core/Models/Context.cs
+++ Core/Models/Context.cs
@@ -15,6 +15,7 @@ namespace Core.Models
         public DbSet<Item> Items { get; set; }
         public DbSet<ItemCategory> ItemCategories { get; set; }
         public DbSet<ItemMovement> ItemMovements { get; set; }
+        public DbSet<ItemTransfer> ItemTransfers { get; set; }
         public DbSet<ItemPromotion> ItemPromotions { get; set; }
         public DbSet<Inventory> Inventories { get; set; }
         public DbSet<Location> Locations { get; set; }
